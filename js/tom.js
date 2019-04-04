8f0df878d74be8d9ad3e6597909f62d1d37ef353
Davis Truong <buckets_truong@Daviss-MacBook-Pro.local>
2019-04-03 22:54:09 -0600

    tamogotchi functionality done, needs styling

--- js/tom.js
+++ js/tom.js
@@ -11,42 +11,88 @@ class Tomagotchi {
 
 $(document).ready(() => {
 
+  let timePassing;
+  let seconds = 0;
+  let startTamogotchi;
+  const secondsGoUp = () => {
+      seconds += 1;
+      $('#time').text("Time " + seconds)
 
+
+      if(startTamogotchi != undefined) {
+        incrementAttributes()
+      }
+
+  }
+  let incrementAttributes = () => {
+    if(seconds % 10 == 0) {
+      startTamogotchi.age += 1
+      //when mario hits age 5
+      if(startTamogotchi.age == 5) {
+        $('#babyMario').css("display", "none")
+        $('#bigMario').show()
+
+      }
+    }
+    if(seconds % 5 == 0) {
+      startTamogotchi.hunger += 1
+    }
+    if(seconds % 7 == 0) {
+      startTamogotchi.bored += 1
+    }
+    if(seconds % 12 == 0) {
+      startTamogotchi.sleep += 1
+    }
+  displayAttributes()
+
+  if(startTamogotchi.hunger == 10 || startTamogotchi.bored == 10 || startTamogotchi.sleep == 10){
+    $('#bigMario').remove()
+    $('#deadMario').show()
+     clearInterval(timePassing)
+     seconds = 0
+     $('time').text("Time" + seconds)
+    }
+  }
+
+  let displayAttributes = () => {
+    $('#stats').remove()
+    let display = "<div id='stats'>"
+    display += "Hunger: " + startTamogotchi.hunger
+    display += "<br />Sleepy: " + startTamogotchi.sleep
+    display += "<br />Bored: " + startTamogotchi.bored
+    display += "<br /> Age: " + startTamogotchi.age
+    display += "</div>"
+    $("#container").append(display)
+  }
 
   $('#lightButton').on('click', (e) => {
+    if(startTamogotchi.sleep > 1) {
+    startTamogotchi.sleep -= 1
+  }
+    displayAttributes()
   })
 
   $('#feedButton').on('click', (e) => {
+    if(startTamogotchi.hunger > 1) {
+      startTamogotchi.hunger -= 1
+    }
+    displayAttributes()
   })
 
   $('#playButton').on('click', (e) => {
-
-  })
-
-  let timePassing;
-  let seconds = 0;
-  let minutes = 0;
-  const secondsGoUp = () => {
-      seconds++;
-      $('#time').text(seconds)
-      if(seconds % 60 === 0){
-          minutes++;
-          $('.minutes').text(minutes);
-      }
+    if(startTamogotchi.bored > 1) {
+    startTamogotchi.bored -= 1
   }
-  $('#startButton').on('click', () => {
-      timePassing = setInterval(secondsGoUp, 1000);
-      let name = $('#name-field').val()
-      let startTamogotchi = new Tomagotchi(name)
-      let display = "Hunger: " + startTamogotchi.hunger
-      display += "<br />Sleepy: " + startTamogotchi.sleep
-      display += "<br />Bored: " + startTamogotchi.bored
-      display += "<br /> Age: " + startTamogotchi.age
-      if(timePassing % 2 == 0) {
-         ++display
-      }
-      $("#container").append(display)
+    displayAttributes()
   })
 
-  secondsGoUp();
+  $('#startButton').on('click', () => {
+      if(startTamogotchi == undefined){
+        timePassing = setInterval(secondsGoUp, 1000);
+        let name = $('#name-field').val()
+        $('#headerName').append(name)
+        startTamogotchi = new Tomagotchi(name)
+        displayAttributes()
+      }
+  })
 })
